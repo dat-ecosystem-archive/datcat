62780cf4d91fca8b69a0db15fb03391c7771a5d7
Max Ogden <max@maxogden.com>
2018-04-10 16:18:28 -0700

    add cli

--- cli.js
+++ cli.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env node
 var first = true
 var stream = require('./')(process.argv[2])
 stream
   .on('data', function (d) {
+    if (Buffer.isBuffer(d)) d = d.toString()
+    if (typeof d === 'object') d = JSON.stringify(d)
     console.log(d)
   })
   .on('end', function () {
